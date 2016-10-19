0d3ce06da37c92a5dfd3ebf85653588db82587bd
ideofunk <ideofunk@gmail.com>
2016-10-18 23:14:27 -0700

    Update blinking_LEDs.py

--- blinking_LEDs.py
+++ blinking_LEDs.py
@@ -4,10 +4,9 @@ import time
 while True:
     for i in range(5):
      x = mraa.Gpio(10 + i) #onboard LED
-   #  x.dir(mraa.DIR_OUT)
-   #  x.write(1)
-   #  time.sleep(1)
-   #  x.write(0)
-   #  time.sleep(1)
-print(x)
+     x.dir(mraa.DIR_OUT)
+     x.write(1)
+     time.sleep(1)
+     x.write(0)
+     time.sleep(1)
      i + 1
