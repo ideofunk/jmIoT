dec75bfc0b5d497cf879fa6a1abc75d91e4db4fc
ideofunk <ideofunk@gmail.com>
2016-10-19 10:04:49 -0700

    Update blinking_LEDs.py

--- blinking_LEDs.py
+++ blinking_LEDs.py
@@ -1,14 +1,15 @@
 import mraa
 import time
 i = 100
-board_LED = mraa.Gpio(i) #onboard LED
-board_LED.dir(mraa.DIR_OUT)
 while True:
+    board_LED = mraa.Gpio(i) #onboard LED
+    board_LED.dir(mraa.DIR_OUT)
     i = i + 1
     board_LED.write(1)
     time.sleep(1)
     board_LED.write(0)
     time.sleep(1)
+    delete
 #    print(i)
     if i >= 105:
         i = 100
