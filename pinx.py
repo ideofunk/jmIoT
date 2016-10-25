23501fa51319fa1d008afde6fdbbe877af9e732c
ideofunk <ideofunk@gmail.com>
2016-10-25 16:34:28 -0700

    Update pinx.py

--- pinx.py
+++ pinx.py
@@ -6,6 +6,6 @@ pin_num = input('Which pin? ') #so I can test different pins with this same file
 pin = mraa.Gpio(pin_num)
 pin.dir(mraa.DIR_IN)
 while True:
-    print('Pin ', pin_num, 'is: ' pin.read())
+    print('Pin ', pin_num, ' is: ' pin.read())
     time.sleep(0.3)
 
