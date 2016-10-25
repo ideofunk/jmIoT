bfc3bc890f3198171e8a5deb15afca406e3a4590
ideofunk <ideofunk@gmail.com>
2016-10-25 15:46:58 -0700

    Update read_35.py

--- read_35.py
+++ read_35.py
@@ -4,8 +4,6 @@ print(mraa.getVersion())
 x = mraa.Gpio(35)
 x.dir(mraa.DIR_IN)
 while True:
-    print("Gpio_35 state:", x.read())
+    print("Gpio_35 state: ", x.read())
     time.sleep(0.3)
-#
-#
-#x.write(1)
+
