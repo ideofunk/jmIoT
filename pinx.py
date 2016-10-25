b1d56b330e9db011db54d791f4e5397b66ee0691
ideofunk <ideofunk@gmail.com>
2016-10-25 16:34:05 -0700

    Update pinx.py

--- pinx.py
+++ pinx.py
@@ -1,10 +1,11 @@
 import mraa
 import time
 print(mraa.getVersion())
-pin_num = input('Which pin? ')
+
+pin_num = input('Which pin? ') #so I can test different pins with this same file
 pin = mraa.Gpio(pin_num)
 pin.dir(mraa.DIR_IN)
 while True:
-    print("Gpio pin state: ", pin.read())
+    print('Pin ', pin_num, 'is: ' pin.read())
     time.sleep(0.3)
 
